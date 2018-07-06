d07741846995e7f4d008ee10b87d2b8c8ffbc0cd
Michael Dragomir <michaeldragomir@mac.com>
2018-07-06 10:19:47 -0700

    additions

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,7 @@
 // This allows us to listen to certain actions and do something when they occur.
 
 // takeEvery takes two arguments. the first one is the action we are listening for.  the second is the generator we want to execute when this action occurs.
-import { takeEvery } from './redux-saga/effects';
+import { takeEvery, all } from './redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga,
@@ -20,14 +20,16 @@ import { purchaseBurgerSaga, fetchOrdersSaga } from './order';
 
 // whenever we execute this generator, we set up a listener, in this case listen for AUTH_INITIATE_LOGOUT, and execute logoutSaga whenever it appears.  we need to import it in the index file.
 export function* watchAuth() {
-  yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-  yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-  // don't forget to import.
-  yield takeEvery(actionTypes.AUTH_CHECK_INITIAL_STATE, authCheckStateSaga);
-  //  we need to import the saga.
-  // we also need to go back to burgerbuilder actions, delete the logic and return a new Action creator.
-  yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
-  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
-    yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
+  yield all ([
+     takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+     takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+     takeEvery(actionTypes.AUTH_USER, authUserSaga),
+    // don't forget to import.
+     takeEvery(actionTypes.AUTH_CHECK_INITIAL_STATE, authCheckStateSaga),
+    //  we need to import the saga.
+    // we also need to go back to burgerbuilder actions, delete the logic and return a new Action creator.
+     takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga),
+     takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga),
+     takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga)
+  ]);
 }
